148b6feb8efa71483a628cba60ec6efc87a4ab86
Alex Munene (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2023-06-24 17:12:26 +0300

    chore: Misc cleanup

--- pkg/xurlfind3r/sources/configuration.go
+++ pkg/xurlfind3r/sources/configuration.go
@@ -4,14 +4,6 @@ import (
 	"regexp"
 )
 
-type Keys struct {
-	GitHub     []string `json:"github"`
-	Intelx     string   `json:"intelx"` // unused, add for the purpose of adding an asterisk `*` on listing sources
-	IntelXHost string   `json:"intelXHost"`
-	IntelXKey  string   `json:"intelXKey"`
-	URLScan    []string `json:"urlscan"`
-}
-
 type Configuration struct {
 	Keys               Keys
 	Domain             string
@@ -21,4 +13,12 @@ type Configuration struct {
 	URLsRegex          *regexp.Regexp
 	MediaURLsRegex     *regexp.Regexp
 	RobotsURLsRegex    *regexp.Regexp
+}
+
+type Keys struct {
+	GitHub     []string `json:"github"`
+	Intelx     string   `json:"intelx"` // unused, add for the purpose of adding an asterisk `*` on listing sources
+	IntelXHost string   `json:"intelXHost"`
+	IntelXKey  string   `json:"intelXKey"`
+	URLScan    []string `json:"urlscan"`
 }
