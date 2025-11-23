148f761f7859e8e0a941be2705d3a9ea564212ee
Alex (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2025-11-23 11:41:56 +0300

    fix(*): log source error if verbose

--- cmd/xurlfind3r/main.go
+++ cmd/xurlfind3r/main.go
@@ -261,11 +261,11 @@ func main() {
 				switch result.Type {
 				case sources.ResultError:
 					if verbose {
-						hqgologger.Error("error finding subdomains!", hqgologger.WithError(err), hqgologger.WithString("source", result.Source))
+						hqgologger.Error("failed to find subdomains!", hqgologger.WithString("source", result.Source), hqgologger.WithError(result.Error))
 					}
 				case sources.ResultURL:
 					if err := writer.Write(output, domain, result); err != nil {
-						hqgologger.Error("error writing subdomain!", hqgologger.WithError(err), hqgologger.WithString("source", result.Source))
+						hqgologger.Error("failed write subdomains!", hqgologger.WithString("source", result.Source), hqgologger.WithError(err))
 					}
 				}
 			}
